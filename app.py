b8417ff49384dad4072efddd2a751a8388a61858
perezchris3599 <kweeziperez712@gmail.com>
2022-07-07 14:21:46 +0300

    added background image

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-
+#background image
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 
