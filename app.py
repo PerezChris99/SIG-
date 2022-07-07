cb85f6546a669fad38ea9885d12d0ad9bebff7ba
perezchris3599 <kweeziperez712@gmail.com>
2022-07-07 14:02:35 +0300

    loaded spaceship images

---
+++ app.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+import random
+import time
+pygame.fint.init()
+
+#loading images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+#player space ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
