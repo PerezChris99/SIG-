b138f45c5b822acbfc9a186ec67dd9abe6f70095
perezchris3599 <kweeziperez712@gmail.com>
2022-07-08 16:22:53 +0300

    added game menu

--- app.py
+++ app.py
@@ -240,7 +240,21 @@ def main():
                 lives -= 1
                 enemies.remove(enemy)
 
-          
         player.move_lasers(-laser_vel, enemies) 
-main()
+def main_menu():
+    title_font = pygame.font.SysFont("comicsans", 40)
+    run = True
+    while run:
+        WIN.blit(BG, (0,0))
+        title_label = title_font.render("Press the mouse to start game", 1, (255,255,255))
+        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+
+main_menu()
 
